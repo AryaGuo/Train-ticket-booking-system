a2863f1a0ee6551aea1fb7a9e1a2af77f0f1ba50
ynny <39652840+jlsbz@users.noreply.github.com>
2018-06-10 10:41:51 +0800

    delete something not used

--- constant.h
+++ constant.h
@@ -11,15 +11,7 @@ namespace sjtu {
     const addType node_utility_byte = 15;       ///  address, isLeaf, K_size, V_size, Ch_size, next. 4 + 1 + 2 + 2 + 2 + 4
 
     const int BlockSize = 4096;                 ///ؽ���Ż�
-
-    const int maxnum_station = 2000;            ///  һ����վ������
-    const int maxnum_loc = 500;                 ///  ��һ��վ����һ��վ�������� ����������֪��׼��׼ȷ��
-
-    const int maxnum_user = 40;                 ///  name       ��󳤶�
-    const int maxnum_other = 20;                ///  password, email, phone, id, loc �ȵ� ��󳤶�
-    const int maxnum_cat = 10;                  ///  catalog          ��󳤶�
-    const int maxnum_ticket = 5;                ///  ticket_type            ��󳤶� (��Ȼ��Щ����д�ţ���û���õ�)
-
+    
     const int TRAIN_CAPACITY = 2000;
 
     const int STRING_LEN = 45;
